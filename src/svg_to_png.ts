332631c4304ad7d49b577ef8ce3cce5e274585ba
猴子 <a102009102009@gmail.com>
2022-08-19 17:20:17 +0800

    remove

--- src/svg_to_png.ts
+++ src/svg_to_png.ts
@@ -4,9 +4,7 @@ import sharp from "sharp";
 glob("assets/svg/*.svg", (_err, files) => {
   for (const file of files) {
     sharp(file)
-      .resize(8, 8)
-      .toFile(`assets/8x8/${file.split("/").pop()?.split(".").shift()}.png`);
-    // .png("assets/8x8");
+      .resize(40, 40)
+      .toFile(`assets/40x40/${file.split("/").pop()?.split(".").shift()}.png`);
   }
 });
-//   sharp("assets/svg", file)
