354453a60b49514cdbae8901a3e040e65d5521bd
猴子 <a102009102009@gmail.com>
2022-10-23 16:01:10 +0800

    feat: twitter emoji

--- src/svg_to_png.ts
+++ src/svg_to_png.ts
@@ -1,3 +1,4 @@
+import fs from "fs";
 import { basename, extname } from "path";
 import glob from "glob";
 import sharp from "sharp";
@@ -6,6 +7,7 @@ glob("assets/svg/*.svg", (_err, files) => {
   for (const file of files) {
     const fileName = basename(file, extname(file));
 
+    fs.mkdirSync("assets/32x32", { recursive: true });
     sharp(file).resize(32, 32).toFile(`assets/32x32/${fileName}.png`);
   }
 });
