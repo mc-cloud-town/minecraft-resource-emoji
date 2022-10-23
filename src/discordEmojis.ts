9ecec17de01dde79d6ef000ed704ca414ea34085
猴子 <a102009102009@gmail.com>
2022-10-23 17:01:58 +0800

    fix

--- src/discordEmojis.ts
+++ src/discordEmojis.ts
@@ -15,6 +15,7 @@ fs.mkdirSync("assets/discordEmojis", { recursive: true });
   let summonIds: string[] = [];
   let old_emoji: typeof emojis = {};
 
+  // 讀取過去緩存資料
   if (fs.existsSync("assets/emoji-checkId.json")) {
     summonIds = JSON.parse(
       fs.readFileSync("assets/emoji-checkId.json", { encoding: "utf8" })
@@ -25,11 +26,25 @@ fs.mkdirSync("assets/discordEmojis", { recursive: true });
       fs.readFileSync("assets/emoji-code.json", { encoding: "utf8" })
     );
   }
+
+  // 獲取表情包資料
   const data = await axios
     .get(`https://discord.com/api/v10/guilds/${GUILD_ID}/emojis`, {
       headers: { Authorization: `Bot ${DISCORD_TOKEN}` },
     })
     .then((res): { id: string; name: string }[] => res.data);
+
+  // 清除資 code 中被清除的 emoji
+  Object.keys(old_emoji).forEach((emojiId) => {
+    if (!data.find(({ id }) => id === emojiId)) {
+      console.log(
+        "delete",
+        data.find(({ id }) => id === emojiId)
+      );
+
+      delete old_emoji[emojiId];
+    }
+  });
 
   let index = data.filter(({ id }) => summonIds.includes(id)).length;
 
@@ -46,10 +61,12 @@ fs.mkdirSync("assets/discordEmojis", { recursive: true });
     };
     console.log(`${name}: ${id}`, snowflakeTime(id));
 
+    // 驗證是否有重複的 emoji
     if (
       (summonIds.includes(id) &&
         !fs.existsSync(`assets/discordEmojis/${id}.png`)) ||
-      !old_emoji?.[id]
+      !old_emoji?.[id] ||
+      summonIds.includes(id)
     ) {
       await writeFile();
 
